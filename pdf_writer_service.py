771e47bb3c8a1ad8bbc380e6a8c40aeab91abdef
William Hill <willhill@google.com>
2021-05-13 21:21:45 -0400

    had to stop, can not get table-from-sheets working inside of Cloud Run

--- pdf_writer_service.py
+++ pdf_writer_service.py
@@ -20,6 +20,8 @@ log_client = google.cloud.logging.Client()
 log_client.get_default_handler()
 log_client.setup_logging()
 
+message = 'START::'
+
 
 def handwriting_gen_file(text, size, deg, height_add=0, reverse=False):
     font = ImageFont.truetype('Cursive.ttf', size)
@@ -33,24 +35,93 @@ def handwriting_gen_file(text, size, deg, height_add=0, reverse=False):
 
 
 def sheets_to_bq(sheets_url, project_id, dataset_id, table_id, schema):
-    bq_client = bigquery.Client()
-    dataset = bq_client.create_dataset(bigquery.Dataset(f'{project_id}.{dataset_id}'), timeout=30, exists_ok=True)
-    bq_client.delete_table(f'{project_id}.{dataset_id}.{table_id}', not_found_ok=True)
+    global message
 
-    table = bigquery.Table(dataset.table('inputs'), schema=schema)
 
-    external_config = bigquery.ExternalConfig(bigquery.ExternalSourceFormat.GOOGLE_SHEETS)
-    external_config.source_uris = [sheets_url]
-    external_config.options.skip_leading_rows = 1
-    external_config.options.range = ('Sheet1')
-    external_config.autodetect = True
-    external_config.schema = schema
+bq_client = bigquery.Client(project_id)
+dataset = bq_client.create_dataset(bigquery.Dataset(f'{project_id}.{dataset_id}'), timeout=30, exists_ok=True)
+bq_client.delete_table(f'{project_id}.{dataset_id}.{table_id}', not_found_ok=True)
 
-    table = bq_client.create_table(table, exists_ok=True)
+table = bigquery.Table(dataset.table('inputs'), schema=schema)
 
-    draw_items = bq_client.query(f'SELECT * FROM {table.dataset_id}.{table.table_id}').to_dataframe().to_dict('records')
+message += f'-- sheets url: {sheets_url} --'
+external_config = bigquery.ExternalConfig(bigquery.ExternalSourceFormat.GOOGLE_SHEETS)
+external_config.source_uris = [sheets_url]
+external_config.options.skip_leading_rows = 1
+external_config.options.range = ('Sheet1')
+external_config.autodetect = True
+external_config.schema = schema
 
-    return draw_items
+table = bq_client.create_table(table, exists_ok=True)
+
+message += f' --QUERY:   SELECT * FROM {table.dataset_id}.{table.table_id}--  '
+draw_items = bq_client.query(f'SELECT * FROM {project_id}.{dataset_id}.{table_id}').to_dataframe().to_dict('records')
+
+message += f' sheets_to_bq()-{len(draw_items)} items '
+
+return draw_items
+
+
+@app.route("/hi", methods=['GET'])
+def hi():
+    global message
+    message += ' start-hi '
+    try:
+        print('print: START - hello')
+
+        logging.info('logging.info: START - hello')
+        logging.debug('logging.info: START - hello')
+
+        message += ' post-logging '
+
+        project_id = request.args.get('project-id')
+        sheets_url = request.args.get('sheet')
+        dataset_id = request.args.get('dataset')
+        table_id = request.args.get('table')
+        pdf_gcs_url = request.args.get('blank-pdf')
+        dest_bucket = request.args.get('bucket')
+
+        message += 'post url-params '
+
+        schema = [bigquery.SchemaField('text', 'String'),
+                  bigquery.SchemaField('x', 'Integer'),
+                  bigquery.SchemaField('y', 'Integer'),
+                  bigquery.SchemaField('font_name', 'String'),
+                  bigquery.SchemaField('font_size', 'Integer'),
+                  bigquery.SchemaField('hand_font_deg', 'Integer'),
+                  bigquery.SchemaField('hand_height_shift', 'Integer'),
+                  bigquery.SchemaField('is_usd', 'Boolean')]
+
+        message += ' post-schema '
+
+        draw_items = sheets_to_bq(sheets_url, project_id, dataset_id, table_id, schema)
+        if len(draw_items) < 1:
+            return f'{message} \n'
+
+        message += ' post-draw_items '
+
+        message += f' pdf_gcs_url = {pdf_gcs_url} '
+
+        with open('blank.pdf', 'wb') as blank_pdf:
+            message += ' create gcs client  '
+            gcs_client = storage.Client()
+            message += ' start download  '
+            gcs_client.download_blob_to_file(pdf_gcs_url, blank_pdf)
+            message += ' download done '
+
+        message += ' post-get-blank '
+
+        written_pdf = write_pdf(draw_items, 'blank.pdf')
+        # os.remove('blank.pdf')
+        message += ' post-write-pdf '
+
+        logging.debug('return PDF')
+        return send_file(written_pdf)
+
+    except Exception as err:
+        message += str(type(err)) + ' : ' + str(err)
+
+    return f'{message}\n'
 
 
 @app.route("/generate-pdf", methods=['GET'])
@@ -89,10 +160,17 @@ def generate_pdf():
 
 
 def write_pdf(draw_items, blank_pdf):
+    global message
+    message += ' write-pdf() '
     packet = io.BytesIO()
     can = canvas.Canvas(packet, pagesize=letter)
 
-    for draw_item in draw_items:
+    message += ' loop-draw-items '
+    message += f' {len(draw_items)} draw_items in list '
+    for i, draw_item in enumerate(draw_items):
+
+        message += f' draw-item[{i}] '
+
         text = draw_item['text']
         if draw_item['is_usd']:
             text = str.rjust(str('${:,.2f}'.format(int(text))), 7)
